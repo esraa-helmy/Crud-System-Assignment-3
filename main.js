84b87532702cd82d02fab371df81a52ae03845ba
esraa-helmy <helmye02@gmail.com>
2024-01-01 19:58:14 +0200

    url validation function

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ var submitBtnEl = document.getElementById("submitBtn");
 var visitBtnEl = document.getElementById("visitBtn");
 var deleteBtnEl = document.getElementById("deleteBtn");
 var tbodyEl = document.getElementById("tbody");
-let nameAlert = document.getElementById("nameAlert")
+let nameAlert = document.getElementById("nameAlert");
+let urlAlert =document.getElementById("urlAlert")
 var container = '';
 var bookMarkList = []
 
@@ -113,13 +114,10 @@ function deleteBookMark (index) {
 }
 
 
-
-
-
 // & Validation Name Function
 function validationName(){
     let regexName = /^[A-Z][a-z]{3,}/
-    let nameRegex = regexName.test(siteNameEl.value)
+    let nameRegex = regexName.test(siteUrlEl.value)
     if(nameRegex==true){
         nameAlert.classList.add('d-none');
         siteNameEl.classList.add('is-valid');
@@ -129,13 +127,28 @@ function validationName(){
         siteNameEl.classList.add('is-invalid');
         nameAlert.classList.remove('d-none');
         siteNameEl.classList.remove('is-valid');
-        
     }
-    
+}
 
+// & Validation WebSite URL function
+function validationWebSiteUrl(){
+    let regexUrl =  /^(https?|ftp):\/\/(([a-z\d]([a-z\d-]*[a-z\d])?\.)+[a-z]{2,}|localhost)(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i;
+    let urlRegex = regexUrl.test(siteUrlEl.value)
+    console.log(urlRegex)
+    if(urlRegex==true){
+        urlAlert.classList.add('d-none');
+        siteUrlEl.classList.add('is-valid');
+        siteUrlEl.classList.remove('is-invalid');
+
+    }else{
+        urlAlert.classList.remove('d-none');
+        siteUrlEl.classList.add('is-invalid');
+        siteUrlEl.classList.remove('is-valid');
+    }
 }
 
 
 // *----------------Events----------------
-submitBtnEl.addEventListener('click',addBookMark)
-siteNameEl.addEventListener('blur',validationName)
+submitBtnEl.addEventListener('click',addBookMark);
+siteNameEl.addEventListener('blur',validationName);
+siteUrlEl.addEventListener('blur',validationWebSiteUrl)
